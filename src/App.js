85a82e270f61a50fc096daff85b74b7efd8c699b
sydmasina <masinasydwell@gmail.com>
2023-07-07 21:27:44 +0200

    Dependencies install

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Navbar from './Components/Navbar/Navabar';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
       </header>
     </div>
   );
