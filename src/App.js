e10835894428bcd109d880573f702c2d0d96cd80
sydmasina <masinasydwell@gmail.com>
2023-07-08 12:46:46 +0200

    Add state logic to to properly navigate between component pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -6,16 +7,40 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Navbar from './Components/Navbar/Navabar';
 import Home from './Components/Home/Home';
+import Projects from './Components/Projects/Projects';
 
-function App() {
+class App extends Component {
+  static defaultProps = {
+    pages: ['Home', 'About', 'Projects', 'Contact']
+  }
+  constructor(props){
+    super(props);
+    this.state = {
+      currentPage: "Home"
+    }
+    this.handleNavclick = this.handleNavclick.bind(this)
+  }
+  handleNavclick(evnt, pageName){
+    console.log("Handling click on parent....")
+    if(pageName !== this.state.currentPage){
+      this.setState({currentPage: pageName})
+    }
+  }
+ render(){
   return (
     <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <Home />
+      <Navbar handleNavclick={this.handleNavclick} />
+      <header  className="App-header">
+        {this.state.currentPage == "Home" ? <Home /> : 
+          this.state.currentPage == "Projects" ? <Projects /> : ''
+        }
+      
+       
       </header>
+      
     </div>
   );
+ }
 }
 
 export default App;
