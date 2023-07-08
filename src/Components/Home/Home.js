64075e1f1481e5610ed102892ca91096f6c82eab
sydmasina <masinasydwell@gmail.com>
2023-07-08 14:43:31 +0200

    Add click event for navigating back to projects

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,17 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 
-function Home() {
+function Home(props) {
+    const {handleNavclick} = props;
         return(
             <Box sx={{ width: '100%', maxWidth: 600 }}>
                  <Typography variant="h3" gutterBottom>
                  Hello there, I'm <Typography color='primary' variant='b'>Syd Masina.</Typography> I am a Full-Stack Software Developer.
                 </Typography>
-                <Button sx={{mt: 2}} size="large" variant="outlined">View my work &raquo;</Button>
+                <Button onClick={(e)=>handleNavclick(e, "Projects")} sx={{mt: 2}} size="large" variant="outlined">View my work &raquo;</Button>
             </Box>
         )
 }
