209dcf9bdba0e2ef167202df70fec4ef879f7e7f
sydmasina <masinasydwell@gmail.com>
2023-07-08 12:43:59 +0200

    Update projectCard component to use handleClick event

--- src/Components/Projects/ProjectCard.js
+++ src/Components/Projects/ProjectCard.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Component } from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -6,37 +7,39 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
 
-export default function ProjectCard() {
-  return (
-    <Card sx={{ 
-        mt:4,
-        maxWidth: 445,
-        backgroundColor: 'primary.main',
-
-        }}
-        color='white'>
-      <CardActionArea  color="primary">
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
-  );
+class ProjectCard extends Component {
+    render(){
+        return (
+            <Card sx={{ 
+                mt:4,
+                maxWidth: 550,
+                backgroundColor: 'primary.main',
+                color: 'white'
+                }}
+                >
+              <CardActionArea  color="primary">
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={this.props.imageLink}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {this.props.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {this.props.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Visit site
+                </Button>
+              </CardActions>
+            </Card>
+          );
+    }
 }
+export default ProjectCard
